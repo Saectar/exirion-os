d0010574e05fcf5aaf55f868300641a4daa96490
rolando.mvs <rolando.mvs@gmail.com>
2014-12-19 17:37:51 +0000

    new

---
+++ program.record.rom.memory/exirion_def.h
@@ -0,0 +1,116 @@
+/*
+** generic header file for ROM exirion-os
+*/
+#if defined(__dsPIC33FJ128GP206__)
+
+#include <p33FJ128GP206.h>
+
+#elif defined(__dsPIC33FJ128GP306__)
+
+#include <p33FJ128GP306.h>
+
+#elif defined(__dsPIC33FJ128GP310__)
+
+#include <p33FJ128GP310.h>
+
+#elif defined(__dsPIC33FJ128GP706__)
+
+#include <p33FJ128GP706.h>
+
+#elif defined(__dsPIC33FJ128GP708__)
+
+#include <p33FJ128GP708.h>
+
+#elif defined(__dsPIC33FJ128GP710__)
+
+#include <p33FJ128GP710.h>
+
+#elif defined(__dsPIC33FJ128MC506__)
+
+#include <p33FJ128MC506.h>
+
+#elif defined(__dsPIC33FJ128MC510__)
+
+#include <p33FJ128MC510.h>
+
+#elif defined(__dsPIC33FJ128MC706__)
+
+#include <p33FJ128MC706.h>
+
+#elif defined(__dsPIC33FJ128MC708__)
+
+#include <p33FJ128MC708.h>
+
+#elif defined(__dsPIC33FJ128MC710__)
+
+#include <p33FJ128MC710.h>
+
+#elif defined(__dsPIC33FJ256GP506__)
+
+#include <p33FJ256GP506.h>
+
+#elif defined(__dsPIC33FJ256GP510__)
+
+#include <p33FJ256GP510.h>
+
+#elif defined(__dsPIC33FJ256GP710__)
+
+#include <p33FJ256GP710.h>
+
+#elif defined(__dsPIC33FJ256MC510__)
+
+#include <p33FJ256MC510.h>
+
+#elif defined(__dsPIC33FJ256MC710__)
+
+#include <p33FJ256MC710.h>
+
+#elif defined(__dsPIC33FJ64GP206__)
+
+#include <p33FJ64GP206.h>
+
+#elif defined(__dsPIC33FJ64GP306__)
+
+#include <p33FJ64GP306.h>
+
+#elif defined(__dsPIC33FJ64GP310__)
+
+#include <p33FJ64GP310.h>
+
+#elif defined(__dsPIC33FJ64GP706__)
+
+#include <p33FJ64GP706.h>
+
+#elif defined(__dsPIC33FJ64GP708__)
+
+#include <p33FJ64GP708.h>
+
+#elif defined(__dsPIC33FJ64GP710__)
+
+#include <p33FJ64GP710.h>
+
+#elif defined(__dsPIC33FJ64MC506__)
+
+#include <p33FJ64MC506.h>
+
+#elif defined(__dsPIC33FJ64MC508__)
+
+#include <p33FJ64MC508.h>
+
+#elif defined(__dsPIC33FJ64MC510__)
+
+#include <p33FJ64MC510.h>
+
+#elif defined(__dsPIC33FJ64MC706__)
+
+#include <p33FJ64MC706.h>
+
+#elif defined(__dsPIC33FJ64MC710__)
+
+#include <p33FJ64MC710.h>
+
+#else
+
+#error -- processor ID not specified in generic header file
+
+#endif
